2323550b13b831401531ff5d71c0b06ce0a9f318
MacBook <macbook@MacBooks-MacBook-Air.local>
2023-05-22 19:33:37 +0300

    final_u

--- pages/favorites.js
+++ pages/favorites.js
@@ -5,7 +5,6 @@ import { setLocalStorage, getLocalStorage } from "../utils/store"
 import { v4 as uuidv4 } from 'uuid'
 import { useState, useEffect } from "react";
 import { Text, Button } from "@mantine/core";
-import { IconSearch } from "@tabler/icons-react";
 import { IconMapPin } from "@tabler/icons-react";
 
 const FavButtondel = (el) => {
@@ -17,7 +16,7 @@ const FavButtondel = (el) => {
 
 
 
-const favorites = () => {
+const Favorites = () => {
 
 
     const [datas, setdatas] = useState([])
@@ -37,20 +36,20 @@ const favorites = () => {
                     <div className={styles.favorite}></div>
                     {
                         datas.length !== 0 ? datas.map((el) => (
-                            <div className={styles.main_info_item}>
+                            <div className={styles.main_info_item} key={uuidv4()}>
                                 <div className={styles.main_info_item1} key={uuidv4()}>
                                     <Text fz="lg" c="#5E96FC" key={uuidv4()}>{el.profession}</Text>
                                     <Button radius="md" variant="subtle" value={el.id}>
                                         <FavButtondel />
                                     </Button>
                                 </div>
-                                <div className={styles.content}>
+                                <div className={styles.content} key={uuidv4()}>
                                     <Text fw={600} key={uuidv4()} c="#232134" >{el.payment_from}</Text>
                                     <Text fw={600} key={uuidv4()} c="#232134" > {el.payment_to}</Text>
                                     <Text fw={600} key={uuidv4()} c="#232134" >{el.currency}</Text> •
                                     <Text key={uuidv4()} className="text">{el.type_of_work.title}</Text>
                                 </div>
-                                <div className="content">
+                                <div className="content" key={uuidv4()}>
                                     <IconMapPin color='#ACADB9' size="1.3rem" />
                                     <Text key={uuidv4()} className="text">{el.town.title}</Text>
                                 </div>
@@ -78,4 +77,4 @@ const favorites = () => {
 
 }
 
-export default favorites
+export default Favorites
